8d7a43cffcf181e36511b808e9883c76718c3fc7
qadirjaved1999 <qadirjaved1999@gmail.com>
2023-11-30 16:15:02 +0500

    magic

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hy Js are you there");
+const blubOn = () => {
+  let getImage = document.getElementById("blub");
+  console.log(getImage);
+  getImage.src = "Images/on.png";
+};
+const blubOff = () => {
+  let getImage = document.getElementById("blub");
+  console.log(getImage);
+  getImage.src = "Images/off.png";
+};
